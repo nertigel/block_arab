95e215a64c71e1d8d8383a5466237fe426ac6f0a
Patrik Nertigel <nertigel14475@gmail.com>
2023-10-25 18:15:11 +0300

    logging and error handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 your_bot_token = "YOUR_TOKEN_HERE"
 
-import re
+import re, logging
 from telegram import Update
 from telegram.ext import (
     Application,
@@ -8,6 +8,10 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 unallowed_emojis = ['🇵🇸', '🇦🇪', '🇪🇬', '🇮🇷', '🇮🇶', '🇯🇴', '🇰🇼', '🇱🇧', '🇸🇦', '🇶🇦', '🇸🇾', '🇾🇪']
 
 async def message_handler(update: Update, context: CallbackContext):
@@ -16,30 +20,30 @@ async def message_handler(update: Update, context: CallbackContext):
     message_id = update.message.message_id
     username = update.message.from_user.username
 
-    print(f"Received message: {message}")
-
     for emoji in unallowed_emojis:
         pattern = re.escape(emoji)
         if re.search(pattern, message):
+            logger.info(f"Message from @{username} with blacklisted emojis at chat: {chat_id}")
             await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
             await context.bot.send_message(chat_id=chat_id, text=f"@{username} has sent blacklisted emojis.")
             break
     
     if re.search('[\u0600-\u06FF]', message):
+        logger.info(f"Message from @{username} with blacklisted characters at chat: {chat_id}")
         await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
         await context.bot.send_message(chat_id=chat_id, text=f"@{username} has sent blacklisted characters.")
 
 def main() -> None:
     """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(your_bot_token).build()
+    if your_bot_token == "":
+        logger.error("No token defined. Please change the string value of `your_bot_token`")
+        return
     
+    logger.info("Starting the bot")
+    application = Application.builder().token(your_bot_token).build()
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler))
-    print("Bot running")
-
-    # Run the bot until the user presses Ctrl-C
     application.run_polling()
-
+    logger.info("ded")
 
 if __name__ == "__main__":
     main()
